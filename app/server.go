dca9dc87d54f311528c3eb12d086894ef4244741
otkamal <me@omar-kamal.com>
2024-04-28 21:38:46 -0700

    first test

--- app/server.go
+++ app/server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strings"
-	"sync"
 )
 
 const BufferSize int = 1024
@@ -13,6 +13,7 @@ const BufferSize int = 1024
 const HttpResponseOk string = "HTTP/1.1 200 OK\r\n"
 const HttpResponseNotFound string = "HTTP/1.1 404 Not Found\r\n\r\n"
 const PlainTextResponse string = "Content-Type: text/plain\r\n"
+const ApplicationResponse string = "Content-Type: application/octet-stream\r\n"
 const ContentLengthResponse string = "Content-Length: "
 
 type HttpRequest struct {
@@ -31,7 +32,8 @@ type HttpResponse struct {
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	wg := sync.WaitGroup{}
+
+	var flag = flag.String("directory", "", "Directory for file")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -46,14 +48,13 @@ func main() {
 			os.Exit(1)
 		}
 
-		go handleClient(conn)
-		wg.Wait()
+		go handleClient(conn, flag)
 
 	}
 
 }
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, baseDirectory *string) {
 
 	defer conn.Close()
 
@@ -68,13 +69,16 @@ func handleClient(conn net.Conn) {
 	fmt.Printf("====== RECEIVED ======\n %v\n\n", buffer[:])
 
 	httpRequest := HttpRequestFromString(strings.TrimSuffix(string(buffer[:]), "\r\n"))
-	test := []byte(CraftHttpResponse(httpRequest))
+	test := CraftHttpResponse(httpRequest, baseDirectory)
+
 	fmt.Printf("\n====== STREAMED ======\n%c\n\n%v\n", test, test)
+
 	_, err = conn.Write(test)
 	if err != nil {
 		fmt.Println("Error Sending:", err)
 		return
 	}
+
 }
 
 func HttpRequestFromString(request string) HttpRequest {
@@ -84,6 +88,7 @@ func HttpRequestFromString(request string) HttpRequest {
 	tokenizedRequest := strings.Split(request, "\r\n")
 	startLine := tokenizedRequest[0]
 
+	// DEBUG MESSAGES
 	fmt.Printf("====== REQUEST ======\n")
 	for i, val := range tokenizedRequest {
 		fmt.Printf("%v: %v", i, val)
@@ -94,6 +99,9 @@ func HttpRequestFromString(request string) HttpRequest {
 	}
 
 	var userAgent string
+	//parsing is messed up and need to start from a later index in a situation where
+	// we are not given headers...
+	//this really needs to be fixed
 	if len(tokenizedRequest) > 4 {
 		userAgent = tokenizedRequest[2]
 		userAgent = strings.Split(userAgent, ":")[1]
@@ -106,10 +114,13 @@ func HttpRequestFromString(request string) HttpRequest {
 
 }
 
-func CraftHttpResponse(request HttpRequest) string {
+func CraftHttpResponse(request HttpRequest, baseDirectory *string) []byte {
+
 	var response HttpResponse
 	var httpResponse string
+
 	switch request.Method {
+
 	case "GET":
 
 		if request.Path == "/" {
@@ -148,6 +159,18 @@ func CraftHttpResponse(request HttpRequest) string {
 					ContentLengthResponse + fmt.Sprintf("%v", response.BodyLength) + "\r\n\r\n" +
 					response.ResponseBody
 
+			} else if tokenizedPath[0] == "files" {
+
+				file, _ := os.ReadFile(*baseDirectory + "/" + tokenizedPath[1])
+				response.StatusLine = HttpResponseOk
+				response.ResponseBody = string(file)
+				response.BodyLength = len(response.ResponseBody)
+
+				httpResponse = response.StatusLine +
+					ApplicationResponse +
+					ContentLengthResponse + fmt.Sprintf("%v", response.BodyLength) + "\r\n\r\n" +
+					response.ResponseBody
+
 			} else {
 				response.StatusLine = HttpResponseNotFound
 				httpResponse = response.StatusLine
@@ -157,5 +180,5 @@ func CraftHttpResponse(request HttpRequest) string {
 
 	}
 
-	return httpResponse
+	return []byte(httpResponse)
 }
